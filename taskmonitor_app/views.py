9d604b9840fd07e72a79dab6b6c9e846bb195d7b
Nathaniel Berube <Rexar14@gmail.com>
2023-10-26 16:39:45 -0500

    Added basic user information and login view WIP

--- taskmonitor_app/views.py
+++ taskmonitor_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+
+
+def login(username):
+    user = User.objects.create_user(username=username, password=password)
