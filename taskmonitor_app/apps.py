032a2ddde53fb85ee96fe275f08fdd3e50c8db3e
Nathaniel Berube <Rexar14@gmail.com>
2023-11-01 20:56:17 -0500

    Updated/Fixed issue with getting the server to run

--- taskmonitor_app/apps.py
+++ taskmonitor_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TaskmonitorConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "taskmonitor"
+    name = "taskmonitor_app"
--- taskmonitor_main/wsgi.py
+++ taskmonitor_main/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaskMonitor.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taskmonitor.settings")
 
 application = get_wsgi_application()
