7fd4f27b72ee2054e498685f47f2b4d373f28c07
Nathaniel Berube <Rexar14@gmail.com>
2023-12-15 19:04:58 -0600

    Minor text fix for calendar url path

--- taskmonitor_app/urls.py
+++ taskmonitor_app/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.register, name="register"),
     path("<str:username>/home/", views.index, name="homepage"),
-    path("<str:username>/calendar", views.calendar_view, name="calendar"),
+    path("<str:username>/calendar/", views.calendar_view, name="calendar"),
 ]
