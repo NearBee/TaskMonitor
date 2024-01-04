6900528ab980fb6a36bf11aa57647e91edc10da0
Nathaniel Berube <Rexar14@gmail.com>
2024-01-03 19:48:56 -0600

    Moved finished script from html side to script.js

--- taskmonitor_app/static/script.js
+++ taskmonitor_app/static/script.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    null //To be added at a later time
+
+    var inputBoxes = document.querySelectorAll('.mb-3 input.forminputBox');
+    var mb3Username = document.querySelector('.mb-3.username');
+
+    if (inputBoxes) {
+        inputBoxes.forEach(function (inputBox) {
+
+            // adding the activeBox class to (currently) the username box when the box is active
+            inputBox.addEventListener('click', function () {
+                var parentBox = inputBox.closest('.mb-3');
+                if (parentBox) {
+                    parentBox.classList.add('activeBox');
+                }
+            });
+
+            // removing the activeBox class to (currently) the username box when the box is inactive
+            inputBox.addEventListener('blur', function () {
+                var parentBox = inputBox.closest('.mb-3');
+                if (parentBox) {
+                    parentBox.classList.remove('activeBox');
+                }
+            });
+        });
+    }
+
 });
