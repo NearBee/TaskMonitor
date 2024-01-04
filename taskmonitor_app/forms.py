308b73482cee7cfa391b81995b45350627af7209
Nathaniel Berube <Rexar14@gmail.com>
2024-01-03 20:26:21 -0600

    Reoredered fields

--- taskmonitor_app/forms.py
+++ taskmonitor_app/forms.py
@@ -7,7 +7,7 @@ from .models import User
 class user_registration_form(forms.ModelForm):
     class Meta:
         model = User
-        fields = "username", "email", "password"
+        fields = "username", "password", "email"
         widgets = {
             "username": TextInput(
                 attrs={
@@ -21,7 +21,7 @@ class user_registration_form(forms.ModelForm):
                     "class": "form-control mb-4 shadow-sm forminputBox",
                     "id": "register_password",
                     "placeholder": "Password",
-                }
+                },
             ),
             "email": EmailInput(
                 attrs={
