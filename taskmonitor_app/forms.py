a7b53a947a64c5690ca13aec625ab2593add4f09
Nathaniel Berube <Rexar14@gmail.com>
2024-01-24 15:10:02 -0600

    Added a confirm password field with a custom clean method

--- taskmonitor_app/forms.py
+++ taskmonitor_app/forms.py
@@ -5,9 +5,20 @@ from .models import User
 
 
 class user_registration_form(forms.ModelForm):
+    password_confirmation = forms.CharField(
+        widget=PasswordInput(
+            attrs={
+                "class": "form-control mb-4 shadow-sm forminputBox",
+                "id": "register_password_confirmation",
+                "placeholder": "Confirm Password",
+            }
+        ),
+        required=True,
+    )
+
     class Meta:
         model = User
-        fields = "username", "password", "email"
+        fields = "username", "password", "password_confirmation", "email"
         widgets = {
             "username": TextInput(
                 attrs={
@@ -31,3 +42,13 @@ class user_registration_form(forms.ModelForm):
                 }
             ),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirmation = cleaned_data.get("password_confirmation")
+
+        if password and password_confirmation and password != password_confirmation:
+            self.add_error("password_confirmation", "Passwords do not match")
+
+        return cleaned_data
